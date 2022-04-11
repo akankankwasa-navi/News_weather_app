2418e98dee3c5b774b1ab152c908b7d71bac7c60
akankankwasa-navi <ivanakankwasa09@gmail.com>
2022-04-12 02:27:46 +0300

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,65 @@
+import Head from "next/head";
+import Banner from "../components/Banner";
+import Header from "../components/Header";
+import ProductFeed from "../components/ProductFeed";
+import { useSession } from "next-auth/client";
+import Footer from "../components/Footer";
+import { getNews } from "../../api/newService";
+import { useState } from "react";
+
+//then here we can destructure props = {products}
+export default function Home() {
+  const [newsFeed,setNewsFeed]=useState([]);
+
+  const getNewsData = async () => {
+
+    const {data:localNews}= await getNews();
+
+    setNewsFeed(localNews.articles);
+    console.log(localNews.articles);
+  
+  }
+
+  getNewsData(); 
+
+ 
+  return (
+    <div className="bg-gray-100">
+      <Head>
+        <title>Amazon By Roland</title>
+      </Head>
+      {/* --This is the header component- */}
+
+      <Header />
+
+      <main className="max-w-screen-2xl mx-auto">
+        {/* ----Banner ---- */}
+        <Banner />
+
+        {/* ----Products feed ---- */}
+        <ProductFeed products={ newsFeed } />
+
+        {/**Footer */}
+        <Footer />
+      </main>
+
+      {/* ---- ---- */}
+    </div>
+  );
+}
+
+
+//https://fakestoreapi.com/products
+// const data = JSON.parse(JSON.stringify(request))
+
+// export async function getServerSideProps(context) {
+//   const products = await fetch("https://fakestoreapi.com/products").then(
+//     (res) => res.json
+//   );
+
+//   return {
+//     props: {
+//       products,
+//     },
+//   };
+// }
